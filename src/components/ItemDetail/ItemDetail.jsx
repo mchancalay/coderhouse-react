d3b728d6e1bed3728c36587584ebda138ee4ca79
Mateo Chancalay <chancalaymateo14@gmail.com>
2023-07-07 22:19:05 -0300

    arreglo la ruta de las imagenes que se rompia

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,10 +4,10 @@ const ItemDetail = ({id, nombre, precio, imagen}) => {
   const alt = `${nombre} imagen`
   return (
     <div>
-      <article className=''>
-        <img className='' src={imagen} alt={alt} />
-        <h3 className=''>{nombre}</h3>
-        <p className=''>{precio}</p>
+      <article className='producto-detail'>
+        <img className='producto-detail__img' src={imagen} alt={alt} />
+        <h3 className='producto-detail__title'>{nombre}</h3>
+        <p className='producto-detail__price'>{precio}</p>
       </article>
     </div>
   )
--- src/js/asyncmock.js
+++ src/js/asyncmock.js
@@ -3,42 +3,42 @@ const productos = [
     "id": "1",
     "nombre": "Buzo Bappe Baby Milo Negro",
     "precio": 1000.00,
-    "imagen": "./src/assets/stock/8.jpg",
+    "imagen": "../src/assets/stock/8.jpg",
     "idCategoria": "3"
   },
   {
     "id": "2",
     "nombre": "Camisa LV Blanca",
     "precio": 1000.00,
-    "imagen": "./src/assets/stock/2.jpg",
+    "imagen": "../src/assets/stock/2.jpg",
     "idCategoria": "2"
   },
   {
     "id": "3",
     "nombre": "Camisa LV Azul",
     "precio": 1000.00,
-    "imagen": "./src/assets/stock/3.jpg",
+    "imagen": "../src/assets/stock/3.jpg",
     "idCategoria": "2"
   },
   {
     "id": "4",
     "nombre": "Chomba Polo Azul",
     "precio": 1000.00,
-    "imagen": "./src/assets/stock/4.jpg",
+    "imagen": "../src/assets/stock/4.jpg",
     "idCategoria": "1"
   },
   {
     "id": "5",
     "nombre": "Chomba Polo Negra",
     "precio": 1000.00,
-    "imagen": "./src/assets/stock/5.jpg",
+    "imagen": "../src/assets/stock/5.jpg",
     "idCategoria": "1"
   },
   {
     "id": "6",
     "nombre": "Chomba Polo Roja",
     "precio": 1000.00,
-    "imagen": "./src/assets/stock/6.jpg",
+    "imagen": "../src/assets/stock/6.jpg",
     "idCategoria": "1"
   }
 ]
